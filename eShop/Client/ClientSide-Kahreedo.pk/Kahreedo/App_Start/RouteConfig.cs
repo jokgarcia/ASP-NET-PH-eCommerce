32c2dc8183321f3a6492377feddc4551670f1f2c
Jok Garcia <ground.guru.jok@outlook.com>
2021-06-12 10:54:02 +0800

    Azure Connection

--- eShop/Client/ClientSide-Kahreedo.pk/Kahreedo/App_Start/RouteConfig.cs
+++ eShop/Client/ClientSide-Kahreedo.pk/Kahreedo/App_Start/RouteConfig.cs
@@ -13,12 +13,19 @@ namespace eCommerceApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                
-            );
+            //);
+
+            routes.MapRoute(
+            "Default",
+            "{controller}/{action}/{id}",
+            new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            new[] { "eCommerceApp.Controllers" }
+);
         }
     }
 }
